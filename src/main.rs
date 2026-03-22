a3107edd5001eef1056822f16708766adb00dde6
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-03-22 18:44:50 +0200

    Add sample code for container discovery

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+mod services;
+
+use crate::services::discovery::{collect_containers_to_backup, get_docker_client};
+
+#[tokio::main]
+async fn main() {
+    if let Ok(docker_client) = get_docker_client() {
+        let containers = collect_containers_to_backup(&docker_client).await;
+        println!("Containers to backup: {:?}", containers);
+    } else {
+        eprintln!("Failed to connect to Docker.");
+    };
 }
