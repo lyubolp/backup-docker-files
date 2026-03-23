6c4e3120f56f3d624e0cb188e5dce3dc8f104176
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-03-23 22:58:58 +0200

    Finish discovery implementation

--- src/services/discovery.rs
+++ src/services/discovery.rs
@@ -2,11 +2,14 @@ use std::collections::HashMap;
 
 use bollard::{Docker, query_parameters::ListContainersOptionsBuilder};
 
+use crate::models::container::Container;
+use crate::models::label::{Label, label_keys};
+
 pub fn get_docker_client() -> Result<Docker, String> {
     Docker::connect_with_unix_defaults().map_err(|e| format!("Failed to connect to Docker: {}", e))
 }
 
-pub async fn collect_containers_to_backup(connection: &Docker) -> Vec<String> {
+pub async fn collect_containers_to_backup(connection: &Docker) -> Vec<Container> {
     let filters = HashMap::from([("label", vec!["bdf.enable"])]);
     let options = ListContainersOptionsBuilder::default()
         .filters(&filters)
@@ -14,13 +17,19 @@ pub async fn collect_containers_to_backup(connection: &Docker) -> Vec<String> {
 
     let containers = connection.list_containers(Some(options)).await;
 
+    let label_keys = label_keys();
     if let Ok(containers) = containers {
         containers
             .into_iter()
             .filter_map(|container| {
                 container.labels.and_then(|labels| {
-                    if labels.get("bdf.enable").map_or(false, |v| v == "true") {
-                        Some(container.names.unwrap()[0].clone().replace("/", ""))
+                    if label_keys.iter().all(|(_, key)| labels.contains_key(key)) {
+                        let label = Label::from_labels(&labels);
+                        Some(Container::new(
+                            container.id.unwrap(),
+                            container.names.unwrap()[0].clone(),
+                            label,
+                        ))
                     } else {
                         None
                     }
