200cbfd44243a0aa2d3956b51d12b7ba65f83fff
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-03-28 18:36:35 +0200

    Update constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,4 @@
 pub const REPO_METADATA_FILE: &str = "repo_metadata.toml";
+pub const CONTAINER_METADATA_FILE: &str = "container_metadata.toml";
+
+pub const STAGING_DIR: &str = "/tmp/bdf-staging"; // What about Windows ?
