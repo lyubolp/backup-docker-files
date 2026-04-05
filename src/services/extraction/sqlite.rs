ef4271bdc6b69d6e14360e9314decf49357c1a83
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-04-05 23:00:11 +0300

    Refactor extract function to remove repository parameter and improve error handling

--- src/services/extraction/sqlite.rs
+++ src/services/extraction/sqlite.rs
@@ -1,12 +1,12 @@
+use std::vec;
+
 use super::utils::create_staging_dir;
 use crate::constants;
 use crate::models::container::Container;
-use crate::models::repo::Repository;
 use crate::services::discovery::get_docker_client;
 use crate::services::extraction::file::copy_file;
 
-
-pub async fn extract(container: &Container, repository: &Repository) -> Result<(), String> {
+pub async fn extract(container: &Container) -> Result<String, String> {
     if let Ok(docker) = get_docker_client() {
         if container.labels.online {
             let pause_result = docker.pause_container(&container.name).await;
@@ -22,13 +22,19 @@ pub async fn extract(container: &Container, repository: &Repository) -> Result<(
 
         let staging_result = create_staging_dir();
 
-        println!("Staging result: {:?}", staging_result);
+        if staging_result.is_err() {
+            return Err(format!(
+                "Failed to create staging directory: {}",
+                staging_result.err().unwrap()
+            ));
+        }
 
         let staging_path = format!("{}/{}", constants::STAGING_DIR, container.name);
 
+        let mut errors: Vec<String> = vec![];
         for path in container.labels.file_paths.iter() {
             if let Err(e) = copy_file(container, path, &staging_path, &docker).await {
-                return Err(format!("Failed to copy file {}: {}", path, e));
+                errors.push(format!("Failed to copy file {}: {}", path, e));
             }
         }
         if container.labels.online {
@@ -42,7 +48,12 @@ pub async fn extract(container: &Container, repository: &Repository) -> Result<(
                 ));
             }
         }
-        Ok(())
+
+        if errors.len() == 0 {
+            Ok(staging_path)
+        } else {
+            Err(errors.join("; "))
+        }
     } else {
         Err("Failed to connect to Docker".to_string())
     }
