3dc90607145561b8b0ab61bd8f7c835e4526b99f
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-04-05 11:04:33 +0300

    Update driver code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
+mod constants;
 mod models;
 mod services;
 
-use crate::services::discovery::{collect_containers_to_backup, get_docker_client};
+use crate::{
+    models::repo::{self, Repository, load_repository, save_repository},
+    services::discovery::{collect_containers_to_backup, get_docker_client},
+};
 
 #[tokio::main]
 async fn main() {
-    if let Ok(docker_client) = get_docker_client() {
-        let containers = collect_containers_to_backup(&docker_client).await;
-        println!("Containers to backup: {:?}", containers);
-    } else {
-        eprintln!("Failed to connect to Docker.");
-    };
+    // if let Ok(docker_client) = get_docker_client() {
+    //     let containers = collect_containers_to_backup(&docker_client).await;
+    //     println!("Containers to backup: {:?}", containers);
+    // } else {
+    //     eprintln!("Failed to connect to Docker.");
+    // };
+
+    let repo = load_repository("temp_repo");
+
+    println!("{:?}", repo);
 }
