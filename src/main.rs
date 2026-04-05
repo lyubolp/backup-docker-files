d96cdf1346000fdc26a8de739fecb2dcf78b46eb
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-04-05 20:03:09 +0300

    Refactor main function to improve error handling and streamline container extraction

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,18 @@ mod services;
 use crate::{
     models::repo::{self, Repository, load_repository, save_repository},
     services::discovery::{collect_containers_to_backup, get_docker_client},
+    services::extraction::sqlite::extract,
 };
 
 #[tokio::main]
 async fn main() {
-    // if let Ok(docker_client) = get_docker_client() {
-    //     let containers = collect_containers_to_backup(&docker_client).await;
-    //     println!("Containers to backup: {:?}", containers);
-    // } else {
-    //     eprintln!("Failed to connect to Docker.");
-    // };
+    let docker_client = get_docker_client().unwrap();
+    let containers = collect_containers_to_backup(&docker_client).await;
+    println!("Containers to backup: {:?}", containers);
 
-    let repo = load_repository("temp_repo");
+    let repo = load_repository("temp_repo").unwrap();
 
-    println!("{:?}", repo);
+    let extract_result = extract(&containers[0], &repo).await;
+
+    println!("Extract result: {:?}", extract_result);
 }
