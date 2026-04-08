397058f48b58810eb88fde88aa5c9ccbe9cad059
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-04-08 19:00:28 +0300

    Walk now accepts a string

--- src/services/extraction/utils.rs
+++ src/services/extraction/utils.rs
@@ -7,7 +7,8 @@ pub fn create_staging_dir() -> Result<(), String> {
     std::fs::create_dir_all(constants::STAGING_DIR).map_err(|e| e.to_string())
 }
 
-pub fn walk_dir(path: &Path) -> Vec<String> {
+pub fn walk_dir(root: &String) -> Vec<String> {
+    let path = Path::new(root);
     let mut result = Vec::new();
     let mut stack = vec![path.to_path_buf()];
 
