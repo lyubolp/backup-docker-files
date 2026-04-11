c1952668e01850aed6293b815c0227c815b67c22
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-04-11 23:53:41 +0300

    SQLite service now backs up all SQLite files and also uses file extraction service

--- src/services/extraction/sqlite.rs
+++ src/services/extraction/sqlite.rs
@@ -1,60 +1,13 @@
-use std::vec;
-
-use super::utils::create_staging_dir;
-use crate::constants;
 use crate::models::container::Container;
-use crate::services::discovery::get_docker_client;
-use crate::services::extraction::file::copy_file;
+use crate::services::extraction::file;
 
 pub async fn extract(container: &Container) -> Result<String, String> {
-    if let Ok(docker) = get_docker_client() {
-        if container.labels.online {
-            let pause_result = docker.pause_container(&container.name).await;
+    // When extracting SQLite databases, we need to copy any "-whl" and "-shm".
+    // This should not be specified in the labels, so we will just add a glob pattern to the label.
 
-            if pause_result.is_err() {
-                return Err(format!(
-                    "Failed to pause container {}: {}",
-                    container.name,
-                    pause_result.err().unwrap()
-                ));
-            }
-        }
+    let mut extended_container = container.clone();
+    extended_container.labels.file_paths[0] =
+        format!("{}*", extended_container.labels.file_paths[0]);
 
-        let staging_result = create_staging_dir();
-
-        if staging_result.is_err() {
-            return Err(format!(
-                "Failed to create staging directory: {}",
-                staging_result.err().unwrap()
-            ));
-        }
-
-        let staging_path = format!("{}/{}", constants::STAGING_DIR, container.name);
-
-        let mut errors: Vec<String> = vec![];
-        for path in container.labels.file_paths.iter() {
-            if let Err(e) = copy_file(container, path, &staging_path, &docker).await {
-                errors.push(format!("Failed to copy file {}: {}", path, e));
-            }
-        }
-        if container.labels.online {
-            let unpause_result = docker.unpause_container(&container.name).await;
-
-            if unpause_result.is_err() {
-                return Err(format!(
-                    "Failed to unpause container {}: {}",
-                    container.name,
-                    unpause_result.err().unwrap()
-                ));
-            }
-        }
-
-        if errors.len() == 0 {
-            Ok(staging_path)
-        } else {
-            Err(errors.join("; "))
-        }
-    } else {
-        Err("Failed to connect to Docker".to_string())
-    }
+    file::extract(&extended_container).await
 }
