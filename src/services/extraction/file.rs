3e5945f5326053c53d7658b7bbce57b372bd34a3
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-04-11 23:53:14 +0300

    Remove staging dir after finishing with a container

--- src/services/extraction/file.rs
+++ src/services/extraction/file.rs
@@ -10,6 +10,7 @@ use super::utils::{create_staging_dir, get_base_from_glob, is_path_glob};
 use crate::constants;
 use crate::models::container::Container;
 use crate::services::discovery::get_docker_client;
+use crate::services::extraction::utils::remove_staging_dir;
 
 pub async fn extract(container: &Container) -> Result<String, String> {
     if let Ok(docker) = get_docker_client() {
@@ -25,14 +26,7 @@ pub async fn extract(container: &Container) -> Result<String, String> {
             }
         }
 
-        let staging_result = create_staging_dir();
-
-        if staging_result.is_err() {
-            return Err(format!(
-                "Failed to create staging directory: {}",
-                staging_result.err().unwrap()
-            ));
-        }
+        create_staging_dir()?;
 
         let staging_path = format!("{}/{}", constants::STAGING_DIR, container.name);
 
@@ -56,6 +50,9 @@ pub async fn extract(container: &Container) -> Result<String, String> {
                 }
             }
         }
+
+        remove_staging_dir()?;
+
         if container.labels.online {
             let unpause_result = docker.unpause_container(&container.name).await;
 
--- src/services/extraction/utils.rs
+++ src/services/extraction/utils.rs
@@ -7,6 +7,10 @@ pub fn create_staging_dir() -> Result<(), String> {
     std::fs::create_dir_all(constants::STAGING_DIR).map_err(|e| e.to_string())
 }
 
+pub fn remove_staging_dir() -> Result<(), String> {
+    std::fs::remove_dir_all(constants::STAGING_DIR).map_err(|e| e.to_string())
+}
+
 pub fn walk_dir(root: &String) -> Vec<String> {
     let path = Path::new(root);
     let mut result = Vec::new();
