46139fe629fcce98308acd9f07e49c9aac7e529d
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-04-11 23:38:49 +0300

    Add support for globs in file extraction

--- src/services/extraction/file.rs
+++ src/services/extraction/file.rs
@@ -1,9 +1,12 @@
 use bollard::Docker;
 use bollard::query_parameters::DownloadFromContainerOptionsBuilder;
 use futures_util::StreamExt;
+use glob::Pattern;
 use std::io::Cursor;
+use std::path;
+use tar::Archive;
 
-use super::utils::create_staging_dir;
+use super::utils::{create_staging_dir, get_base_from_glob, is_path_glob};
 use crate::constants;
 use crate::models::container::Container;
 use crate::services::discovery::get_docker_client;
@@ -35,8 +38,22 @@ pub async fn extract(container: &Container) -> Result<String, String> {
 
         let mut errors: Vec<String> = vec![];
         for path in container.labels.file_paths.iter() {
-            if let Err(e) = copy_file(container, path, &staging_path, &docker).await {
-                errors.push(format!("Failed to copy file {}: {}", path, e));
+            if is_path_glob(path) {
+                let glob_root = get_base_from_glob(path);
+                let matches =
+                    expand_glob_in_container(path, &glob_root, container, &docker).await?;
+
+                for matched_path in matches {
+                    if let Err(e) =
+                        copy_file(container, &matched_path, &staging_path, &docker).await
+                    {
+                        errors.push(format!("Failed to copy file {}: {}", matched_path, e));
+                    }
+                }
+            } else {
+                if let Err(e) = copy_file(container, path, &staging_path, &docker).await {
+                    errors.push(format!("Failed to copy file {}: {}", path, e));
+                }
             }
         }
         if container.labels.online {
@@ -67,6 +84,18 @@ pub async fn copy_file(
     target_path: &str,
     docker: &Docker,
 ) -> Result<(), String> {
+    let mut archive = prepare_tar_with_contents(container, source_path, docker).await?;
+
+    archive.unpack(target_path).map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
+async fn prepare_tar_with_contents(
+    container: &Container,
+    source_path: &str,
+    docker: &Docker,
+) -> Result<Archive<Cursor<Vec<u8>>>, String> {
     let options = DownloadFromContainerOptionsBuilder::new()
         .path(source_path)
         .build();
@@ -80,9 +109,52 @@ pub async fn copy_file(
     }
 
     let cursor = Cursor::new(buffer);
-    tar::Archive::new(cursor)
-        .unpack(target_path)
-        .map_err(|e| e.to_string())?;
+    Ok(tar::Archive::new(cursor))
+}
 
-    Ok(())
+async fn expand_glob_in_container(
+    pattern: &str,
+    source_path: &str,
+    container: &Container,
+    docker: &Docker,
+) -> Result<Vec<String>, String> {
+    let mut archive = prepare_tar_with_contents(container, source_path, docker).await?;
+
+    let paths: Vec<String> = archive
+        .entries()
+        .unwrap()
+        .map(|e| e.unwrap().path().unwrap().display().to_string())
+        .collect();
+
+    let reanchored_paths = reanchor_paths(&paths, source_path);
+
+    let glob_pattern = Pattern::new(pattern).map_err(|e| e.to_string())?;
+
+    Ok(reanchored_paths
+        .into_iter()
+        .filter(|path| glob_pattern.matches(path))
+        .collect::<Vec<_>>())
+}
+
+fn reanchor_paths(paths: &Vec<String>, source_path: &str) -> Vec<String> {
+    /*
+    Paths received from the Docker API are anchored at the base of the path.
+    For example: ["dir/file1.txt", "dir/file2.txt"], instead of "/data/dir/file1.txt", "/data/dir/file2.txt".
+    Source path in this case is "/data/dir", and we will have a glob pattern like "/data/dir/\*.txt" (Ignore the escape)
+    */
+
+    // Get the last part of the source path, which is the anchor for the paths received from Docker (e.g. "dir" in the example above)
+    let last_dir = path::Path::new(source_path)
+        .components()
+        .last()
+        .unwrap()
+        .as_os_str()
+        .to_str()
+        .unwrap();
+
+    // Replace the anchor with the full source path to get the correct paths (e.g. replace "dir/file1.txt" with "/data/dir/file1.txt")
+    paths
+        .into_iter()
+        .map(|p| p.replacen(last_dir, source_path, 1))
+        .collect()
 }
